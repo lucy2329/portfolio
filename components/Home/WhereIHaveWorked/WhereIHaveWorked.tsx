9e8c8d42b8e9989c9d19cedb9626c455878b5936
clearly-outsane <1starwarrior516@gmail.com>
2023-07-09 12:22:45 +0530

    UGHHHHHHHHHHHHH

--- components/Home/WhereIHaveWorked/WhereIHaveWorked.tsx
+++ components/Home/WhereIHaveWorked/WhereIHaveWorked.tsx
@@ -148,10 +148,10 @@ const CompaniesBar = (props) => {
             setDescriptionJob={props.setDescriptionJob}
           />
         </div>
-        <div className='block md:hidden w-[380px] h-0.5 rounded bg-gray-500'>
+        <div className='block md:hidden w-[576px] h-0.5 rounded bg-gray-500'>
           <motion.div
             animate={{ x: barAbovePosition }}
-            className='w-[128px] h-0.5 rounded bg-AAsecondary'
+            className='w-[192px] h-0.5 rounded bg-AAsecondary'
           ></motion.div>
         </div>
       </div>
